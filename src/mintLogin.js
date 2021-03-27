ef7c3f24d192a2352120d47886e9f5b4c89a65e2
vagrant <vagrant@vagrant.vm>
2021-03-27 03:03:08 +0000

    2fa for mint

--- src/mintLogin.js
+++ src/mintLogin.js
@@ -1,4 +1,8 @@
 const puppeteer = require('puppeteer');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 (async () => {
   const browser = await puppeteer.launch(
@@ -11,28 +15,40 @@ const puppeteer = require('puppeteer');
       slowMo: 20
     });
   const page = await browser.newPage();
-  // await page.goto("https://accounts.intuit.com/");
-  // await page.type('#ius-identifier', 'env.email', { delay: 100 });
-  // await page.click('#ius-sign-in-submit-btn')
-  // await page.waitForSelector('#ius-sign-in-mfa-password-collection-current-password');
-  // await page.waitForTimeout(20000);
-  // await page.type('#ius-sign-in-mfa-password-collection-current-password', 'env.pass');
-  // await page.click('#ius-sign-in-mfa-password-collection-continue-btn');
-  // await page.waitForTimeout(20000);
-  // await page.screenshot({ path: 'example.png' });
+  await page.goto("https://accounts.intuit.com/");
+  await page.type('#ius-identifier', `${process.env.email}`, { delay: 100 });
+  await page.click('#ius-sign-in-submit-btn');
+  await page.waitForSelector('#ius-sign-in-mfa-password-collection-current-password');
+  await page.waitForTimeout(20000);
+  await page.type('#ius-sign-in-mfa-password-collection-current-password', `${process.env.pass}`);
+  await page.click('#ius-sign-in-mfa-password-collection-continue-btn');
+  console.log("pass entered...waiting");
+  // await page.waitForTimeout(12000);
+  console.log("done waiting, taking screenshot");
+  await page.screenshot({ path: 'example.png' });
+  console.log("redirecting to mint");
+  await page.goto('https://mint.intuit.com/transaction.event?startDate=03/01/2021&endDate=03/31/2021');
+  await page.waitForTimeout(5000);
+  await page.click('#transactionExport');
 
-  await page.goto('https://jsonplaceholder.typicode.com/todos/1')
-  var innerText = await page.evaluate(() =>  {
-    return JSON.parse(document.querySelector('body').innerText);
-  });
+  // await page.goto('https://jsonplaceholder.typicode.com/todos/1')
+  // var innerText = await page.evaluate(() => {
+  //   return JSON.parse(document.querySelector('body').innerText);
+  // });
 
-  console.log(innerText);
+  // console.log(innerText);
+  const name = await new Promise((resolve) => {
+    readline.question("Enter OTP Code: ", answer => {
+      resolve(answer);
+      readline.close();
+    })
+  })
 
+  console.log(`Your name is ${name}`);
 
-
-// Need to figure out the 2 factor Auth. Maybe need to load it once and manually type it in?
-// Once authenticated and at home page. can i just do await.page.goto(<directurlfortransactions>)? Not sure how the authentication is carried over. I guess it is
-// all in one session....
+  // Need to figure out the 2 factor Auth. Maybe need to load it once and manually type it in?
+  // Once authenticated and at home page. can i just do await.page.goto(<directurlfortransactions>)? Not sure how the authentication is carried over. I guess it is
+  // all in one session....
 
 
   await browser.close();
