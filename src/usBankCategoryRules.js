6a2fd72c5fa414e3fab728af10a625df71df206e
vagrant <vagrant@vagrant.vm>
2021-05-18 02:32:54 +0000

    switch usbank venmo to shopping budget to align with mint rules

--- src/usBankCategoryRules.js
+++ src/usBankCategoryRules.js
@@ -2,7 +2,7 @@ const applyUSBankCategoryRules = (element) => {
     if(element.Name.includes("VANGUARD BUY")) return "Amortized"
 
     if(element.Name.includes("VENMO") && element.Amount.includes("-125.00")) return "HouseCleaning"
-    if(element.Name.includes("VENMO")) return "Shopping"
+    if(element.Name.includes("VENMO")) return "ShoppingBudget"
     if(element.Name.includes("IOWA STATE ED AS")) return "UnionDues"
     if(element.Name.includes("CHECK") && element.Amount.includes("-90.00")) return 'Amortized'
     if(element.Name.includes("MONTHLY MAINTENANCE FEE")) return "Miscellaneous"
