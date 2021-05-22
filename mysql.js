9122fdc78236412b469a4542acb817317369c585
vagrant <vagrant@vagrant.vm>
2021-05-22 02:52:31 +0000

    bulk insert of transformed data

--- mysql.js
+++ mysql.js
@@ -1,25 +1,49 @@
 const mariadb = require('mariadb');
+const { readFile } = require('./src/readFile');
+const { transformDataMint } = require('./src/mintTransform');
+const { setFile } = require('./src/setFile');
+
+COLUMNS = ['date', 'description', 'amount', 'category', 'label', 'notes', 'transaction_type', 'original_description', 'account', 'budget_category']
+
+
+
 const pool = mariadb.createPool({
-     user: process.env.DB_USER, 
-     password: process.env.DB_PASS,
-     database: process.env.DB_DATABASE,
-     connectionLimit: 5
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  connectionLimit: 5
 });
 async function asyncFunction() {
+  let dataMint = await readFile(`${__dirname}/src/input/mint/transactions_input_0421.csv`);
+  let transformedDataMint = transformDataMint(dataMint);
+
+  let data = transformedDataMint.split('\n');
+  data.shift();
+  let dataArray = [];
+
+  data.forEach(element => {
+    items = element.split(",")
+    dataArray.push(items);
+    console.log(items);
+  })
+
+
+  let stmt = `INSERT INTO transactions(date, description, amount, category, label, notes, transaction_type, original_description, account, budget_category) VALUES (?,?,?,?,?,?,?,?,?,?)`;
+
+
   let conn;
   try {
-	conn = await pool.getConnection();
-	const rows = await conn.query("SELECT 1 as val");
-	console.log(rows); //[ {val: 1}, meta: ... ]
-	const res = await conn.query("INSERT INTO mytable (a) value ('G')", [1, "mariadb"]);
-	console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-
+    conn = await pool.getConnection();
+    const res = await conn.batch(stmt, dataArray);
+    console.log(res);
   } catch (err) {
-      console.error(err);
-	throw err;
+    console.error(err);
+    throw err;
   } finally {
-	if (conn) return conn.end();
+    if (conn) return conn.end();
   }
 }
 
+
 asyncFunction();
+
