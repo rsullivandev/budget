a4f20a2451a21710323ad1cee8020c0ceb37390d
vagrant <vagrant@vagrant.vm>
2021-05-29 02:49:46 +0000

    server boilerplate

--- src/server.js
+++ src/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3005
 
+// app.use(express.urlencoded());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello!');
+    res.sendFile(`${__dirname}/views/index.html`);
+})
+
+app.post('/files/new', (req, res) => {
+    console.log("got body: ", req.body);
+    res.sendStatus(200);
 })
 
 app.listen(port, () => {
