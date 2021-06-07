648c48e148717ce82fe244ea65552379e8f6380b
vagrant <vagrant@vagrant.vm>
2021-06-07 03:05:28 +0000

    update usbank rules

--- src/services/usBankCategoryRules.js
+++ src/services/usBankCategoryRules.js
@@ -6,6 +6,7 @@ const applyUSBankCategoryRules = (element) => {
     if(element.Name.includes("IOWA STATE ED AS")) return "UnionDues"
     if(element.Name.includes("CHECK") && element.Amount.includes("-90.00")) return 'Amortized'
     if(element.Name.includes("MONTHLY MAINTENANCE FEE")) return "Miscellaneous"
+    if(element.Name.includes("SALES TAX")) return "Miscellaneous"
     if(element.Transaction.includes("CREDIT")) return "Income"
     else return "Miscellaneous"
 }
