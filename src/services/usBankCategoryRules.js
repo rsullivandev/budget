d570a8622501b8728fd7ba74af7ec4bdebf53c59
vagrant <vagrant@vagrant.vm>
2021-07-08 02:55:45 +0000

    update hair to 95

--- src/services/usBankCategoryRules.js
+++ src/services/usBankCategoryRules.js
@@ -4,7 +4,7 @@ const applyUSBankCategoryRules = (element) => {
     if(element.Name.includes("VENMO") && element.Amount.includes("-125.00")) return "HouseCleaning"
     if(element.Name.includes("VENMO")) return "ShoppingBudget"
     if(element.Name.includes("IOWA STATE ED AS")) return "UnionDues"
-    if(element.Name.includes("CHECK") && element.Amount.includes("-90.00")) return 'Amortized'
+    if(element.Name.includes("CHECK") && element.Amount.includes("-95.00")) return 'Amortized'
     if(element.Name.includes("MONTHLY MAINTENANCE FEE")) return "Miscellaneous"
     if(element.Name.includes("SALES TAX")) return "Miscellaneous"
     if(element.Transaction.includes("CREDIT")) return "Income"
