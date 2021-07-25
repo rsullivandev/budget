6f07c4f963a273588b42bb0a664ab46dca1b7e18
vagrant <vagrant@vagrant.vm>
2021-07-25 19:50:03 +0000

    display friendly date

--- src/client/screens/BudgetScreen.js
+++ src/client/screens/BudgetScreen.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
 
 const columns = [
-    { field: 'id', headerName: 'Id', width: 150 },
-    { field: 'date', headerName: 'Date', width: 150 },
-    { field: 'planned', headerName: 'Planned Amount', width: 150 },
-    { field: 'actual', headerName: 'Actual Amount', width: 150 },
-    { field: 'net', headerName: 'Net Amount', width: 150 },
+    { field: 'id', headerName: 'Id', description: "A unique identifier for this budget", flex: .2},
+    { field: 'date', headerName: 'Date', descrition: "The month this budget is effective for - set for a given month and year.", flex: .2,
+        valueFormatter: (params) => {
+            const date = new Date(params.value);
+            return `${date.getUTCMonth()+1}/${date.getUTCFullYear()}`
+        }},
+    { field: 'planned', headerName: 'Planned Amount', description: "The total amount planned to be spent for this budget", flex: .2},
+    { field: 'actual', headerName: 'Actual Amount', description: "The actual amount spent for this budget", flex: .2},
+    { field: 'net', headerName: 'Net Amount', description: "The net difference between planned and actual for this budget", flex: .2},
 
 ];
 
@@ -22,12 +26,12 @@ export default class BudgetScreen extends React.Component {
     componentDidMount = async () => {
         try {
             const data = await (await fetch('/api/budgetHeaders')).json();
-            const newArray = data.map(item => {
-                return {id: item.id, date: item.date, planned: "", actual: "", net: ""}
+            const filteredArray = data.map(item => {
+                return { id: item.id, date: item.date, planned: "", actual: "", net: "" }
             })
-            console.log(newArray);
+            console.log(filteredArray);
             this.setState({
-                budget: newArray
+                budget: filteredArray
             })
         } catch (e) {
             console.log(e);
@@ -37,12 +41,13 @@ export default class BudgetScreen extends React.Component {
 
     render() {
         const { budget } = this.state;
-        const {rows} = this.state;
-        console.log(budget);
         return (
-            <div style={{ height: 300, width: '100%' }}>
-                <DataGrid columns={columns} rows= {budget}
-                />
+            <div style={{ height: 400, width: '100%' }}>
+                <div style={{ display: 'flex', height: '100%' }}>
+                    <div style={{ flexGrow: 1 }}>
+                        <DataGrid columns={columns} rows={budget}/>
+                    </div>
+                </div>
             </div >
         )
     }
