21406e17dd67ccd0d2c6eea8ccaf47e9b7203be9
vagrant <vagrant@vagrant.vm>
2021-08-20 03:01:07 +0000

    category screen

--- src/client/screens/CategoryScreen.js
+++ src/client/screens/CategoryScreen.js
@@ -1,20 +1,58 @@
 import React from 'react'
+import { DataGrid } from '@material-ui/data-grid';
+import { Button } from '@material-ui/core';
+
+const columns = [
+    { field: 'id', headerName: 'Id', description: "A unique identifer for this budget category", flex: 1 },
+    { field: "categoryName", headerName: "Category Name", description: "The name of the budget category", flex: 1 },
+    { field: "description", headerName: "Description", description: "The description of the category", flex: 1 },
+    { field: "budgetType", headerName: "Type", description: "Monthly budget or escrow", flex: 1 }
+]
 
 export default class CategoryScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            transactions: []
+            categories: []
         }
+    }
+
+    componentDidMount = async () => {
+        try {
+            const data = await (await fetch('/api/categories')).json();
+            this.setState({
+                categories: data
+            })
+
+            //TODO - Sort by Type 
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    handleClick = async (event) => {
+        console.log("Clicked!");
+        // this.props.props.setSelectedBudget(event.id)
+    }
+
+    handleSubmit = async () => {
+        console.log("submit!");
     }
 
 
     render() {
+        const { categories } = this.state;
         return (
-            <div>
-                <h3>This is the Category Screen</h3>
-                <p>In the future, you will be able to maintain budget categories for use in each monthly budget</p>
-            </div>
+            <div style={{ height: 800, width: '100%' }}>
+                <div style={{ display: 'flex', height: '100%' }}>
+                    <div style={{ flexGrow: 1 }}>
+                        <h2>Categories</h2>
+                        <DataGrid columns={columns} rowHeight={25} rows={categories} onRowClick={this.handleClick} />
+                        <Button style={{ marginLeft: '20px' }} variant="contained" color='primary' onClick={this.handleSubmit}>Submit</Button>
+                    </div>
+                </div>
+            </div >
         )
     }
 }
